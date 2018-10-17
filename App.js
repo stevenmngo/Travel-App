cbb43a7e5c818f11547340e66fd8872b636c6da0
PhucLe <bobgel12@gmail.com>
2018-10-16 20:33:07 -0700

    Revert "Create Tab Navigation for The new Trip"
    This reverts commit 97ef65ba9ed949ae30c9ed29e79c60fe80b596ab.

--- App.js
+++ App.js
@@ -5,14 +5,6 @@ import HomeScreen from './screens/HomeScreen'
 import SettingScreen from './screens/SettingScreen'
 import SavedTripScreen from './screens/SavedTripScreen'
 import DayDetailScreen from './screens/DayDetailScreen'
-import DayPickerScreen from './screens/DayPickerScreen'
-import SignInScreen from './screens/SignInScreen'
-import RegisterScreen from './screens/RegisterScreen'
-
-
-// React-Navigation-Material
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
-
 
 // Redux Import
 import reduxStore from './store'
@@ -30,7 +22,7 @@ export default class App extends React.Component {
   }
 }
 
-// This is the Drawer Navigator
+
 const CustomDrawComponent = (props) => (
   <SafeAreaView style = {{flex: 1}}>
     <View style = {{height: 150, backgroundColor: 'white', alignItems: 'center', justifyContent:'center'}}>
@@ -42,21 +34,11 @@ const CustomDrawComponent = (props) => (
   </SafeAreaView>
 )
 
-// This is the tab Navigator for NewTrip
-const TabNavigation = createMaterialBottomTabNavigator({
-  Destination: { screen: HomeScreen },
-  DayPicker: { screen: DayPickerScreen },
-  DayDetail: { screen: DayDetailScreen }
-})
-
-// This is the main Navigator for the app
 const AppDrawNavigator = createDrawerNavigator({
   Home: HomeScreen,
   Setting: SettingScreen,
-  Register: RegisterScreen,
-  SignIn: SignInScreen,
-  SavedTrip: SavedTripScreen,
-  NewTrip: TabNavigation
+  DayDetail: DayDetailScreen,
+  SavedTrip: SavedTripScreen
 },{
   contentComponent: CustomDrawComponent,
   // drawerWidth: width
@@ -64,3 +46,12 @@ const AppDrawNavigator = createDrawerNavigator({
     activeTintColor: "orange"
   }
 })
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
