1b6f5478fa4b8f41d0e3cbc5cd738bff18dc951a
PhucLe <bobgel12@gmail.com>
2018-11-20 01:22:25 -0800

    Add Day Picker

--- screens/DayPickerScreen.js
+++ screens/DayPickerScreen.js
@@ -3,65 +3,67 @@ import { View, Text, StyleSheet, FlatList, TextInput, TouchableOpacity } from 'r
 import { Button, Header, Left, Right, Icon, Item, Body, Title, Thumbnail } from 'native-base'
 
 import DateTimePicker from 'react-native-modal-datetime-picker';
+import DatepickerRange from 'react-native-range-datepicker';
 
 class DayPickerScreen extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            isDateTimePickerVisibleStart: false,
-            isDateTimePickerVisibleEnd: false,
-            startDate: new Date().toLocaleString(),
-            endDate: new Date().toLocaleString(),
+            // isDateTimePickerVisibleStart: false,
+            // isDateTimePickerVisibleEnd: false,
+            startDate: new Date(),
+            untilDate: new Date(),
+            // endDate: new Date().toLocaleString(),
             totalDays: 0
         };
         // These line is to bind this key word
-        this._showDateTimePickerStart = this._showDateTimePickerStart.bind(this);
-        this._showDateTimePickerEnd = this._showDateTimePickerEnd.bind(this);
-        this._hideDateTimePickerStart = this._hideDateTimePickerStart.bind(this);
-        this._hideDateTimePickerEnd = this._hideDateTimePickerEnd.bind(this);
-        this._handleDatePickedStart = this._handleDatePickedStart.bind(this);
-        this._handleDatePickedEnd = this._handleDatePickedEnd.bind(this);
-        this._updateTodayDay = this._updateTodayDay.bind(this);
+        // this._showDateTimePickerStart = this._showDateTimePickerStart.bind(this);
+        // this._showDateTimePickerEnd = this._showDateTimePickerEnd.bind(this);
+        // this._hideDateTimePickerStart = this._hideDateTimePickerStart.bind(this);
+        // this._hideDateTimePickerEnd = this._hideDateTimePickerEnd.bind(this);
+        // this._handleDatePickedStart = this._handleDatePickedStart.bind(this);
+        // this._handleDatePickedEnd = this._handleDatePickedEnd.bind(this);
+        // this._updateTodayDay = this._updateTodayDay.bind(this);
     }
 
-    // These function is being used to control the datePicker
-    _showDateTimePickerStart = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleStart: true }));
-    _showDateTimePickerEnd = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleEnd: true }));
-    _hideDateTimePickerStart = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleStart: false }));
-    _hideDateTimePickerEnd = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleEnd: false }));
+    // // These function is being used to control the datePicker
+    // _showDateTimePickerStart = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleStart: true }));
+    // _showDateTimePickerEnd = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleEnd: true }));
+    // _hideDateTimePickerStart = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleStart: false }));
+    // _hideDateTimePickerEnd = () => this.setState(Object.assign(this.state, { isDateTimePickerVisibleEnd: false }));
 
-    // This function calculate the total days of the trips
-    _updateTodayDay = () => {
-        if (this.state.startDate != "" && this.state.endDate != ""){
-            let startDateStr = this.state.startDate.split(',')[0]
-            startDate = new Date(startDateStr)
-            let endDateStr = this.state.endDate.split(',')[0]
-            endDate = new Date(endDateStr)
-            if (endDate > startDate){
-                var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
-                var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-                this.setState(Object.assign(this.state, { totalDays: diffDays}))
-            } else {
-                this.setState(Object.assign(this.state, { totalDays: 0}))
-            }
-        }
-    }
+    // // This function calculate the total days of the trips
+    // _updateTodayDay = () => {
+    //     if (this.state.startDate != "" && this.state.endDate != ""){
+    //         let startDateStr = this.state.startDate.split(',')[0]
+    //         startDate = new Date(startDateStr)
+    //         let endDateStr = this.state.endDate.split(',')[0]
+    //         endDate = new Date(endDateStr)
+    //         if (endDate > startDate){
+    //             var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+    //             var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    //             this.setState(Object.assign(this.state, { totalDays: diffDays}))
+    //         } else {
+    //             this.setState(Object.assign(this.state, { totalDays: 0}))
+    //         }
+    //     }
+    // }
 
-    // Function handle Start Day confirm
-    _handleDatePickedStart = (date) => {
-        console.log('A date has been picked: ', date);
-        this.setState(Object.assign(this.state, { startDate: date.toLocaleString()}))
-        this._hideDateTimePickerStart()
-        this._updateTodayDay()
-    };
+    // // Function handle Start Day confirm
+    // _handleDatePickedStart = (date) => {
+    //     console.log('A date has been picked: ', date);
+    //     this.setState(Object.assign(this.state, { startDate: date.toLocaleString()}))
+    //     this._hideDateTimePickerStart()
+    //     this._updateTodayDay()
+    // };
 
-    // Function handle End Day confirm
-    _handleDatePickedEnd = (date) => {
-        console.log('A date has been picked: ', date);
-        this.setState(Object.assign(this.state, { endDate: date.toLocaleString()}))
-        this._hideDateTimePickerEnd()
-        this._updateTodayDay()
-    };
+    // // Function handle End Day confirm
+    // _handleDatePickedEnd = (date) => {
+    //     console.log('A date has been picked: ', date);
+    //     this.setState(Object.assign(this.state, { endDate: date.toLocaleString()}))
+    //     this._hideDateTimePickerEnd()
+    //     this._updateTodayDay()
+    // };
 
     render() {
         return (
@@ -80,11 +82,28 @@ class DayPickerScreen extends Component {
                         </Right>
                     </Header>
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>Total Days</Text>
-                    <Text>{this.state.totalDays}</Text>
-                </View>
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>Start Date</Text>
+                    {/* <Text>Total Days</Text>
+                    <Text>{this.state.startDate.toLocaleString()}</Text>
+                    <Text>{this.state.untilDate.toLocaleString()}</Text> */}
+                    {/* <DatepickerRange>
+                        startDate: '13052017',
+                        untilDate: '26062017',
+                        onConfirm: {( startDate, untilDate ) => this.setState({ startDate, untilDate })}
+                        selectedBackgroundColor: 'red',
+	                    selectedTextColor: 'white',
+	                    todayColor: 'red',
+                        buttonColor: 'red',
+                    </DatepickerRange> */}
+                    <DatepickerRange
+                        selectedBackgroundColor= 'red'
+                        selectedTextColor= 'white'
+                        todayColor= 'red'
+                        buttonColor= 'red'
+                        startDate = '13052017'
+                        untilDate = '26062017'
+                        onConfirm = {(startDate, untilDate) => this.setState({ startDate, untilDate })}
+                    />
+                    {/* <Text>Start Date</Text>
                     <TouchableOpacity onPress={this._showDateTimePickerStart}>
                         <Text>{this.state.startDate}</Text>
                     </TouchableOpacity>
@@ -101,7 +120,7 @@ class DayPickerScreen extends Component {
                         isVisible={this.state.isDateTimePickerVisibleStart}
                         onConfirm={this._handleDatePickedStart}
                         onCancel={this._hideDateTimePickerStart}
-                    />
+                    /> */}
                 </View>
             </View>
         )
