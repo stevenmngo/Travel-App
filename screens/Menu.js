aefe8a858bf974f803ed33c6e1afc2e11643d95d
Kat1803 <kathy.luu0909@gmail.com>
2018-11-29 10:07:06 -0800

    Revert "Make pop-up button"
    This reverts commit 1c9ee871ea6547e7ae5d59d39626a29d0cfa417a.

--- screens/Menu.js
+++ screens/Menu.js
@@ -70,9 +70,9 @@ const TabNavigation = createMaterialBottomTabNavigator({
 const AppDrawNavigator01 = createDrawerNavigator(
   {
     Home: HomeScreen,
-    "New Trip": TabNavigation,
-    "Saved Trip": SavedTripScreen,
-    "Sign In": Tab,
+    New_Trip: TabNavigation,
+    Saved_Trip: SavedTripScreen,
+    Sign_In: Tab,
     About: AboutScreen,
   },
 
